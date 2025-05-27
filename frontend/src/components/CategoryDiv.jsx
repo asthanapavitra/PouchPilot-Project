d4cbae4ed5d773203e5cb9faf596bddee49a21d7
Pavitra Asthana <asthanapavitra@gmail.com>
2025-05-27 15:32:53 +0530

    Added Loading Msg to home page

--- frontend/src/components/CategoryDiv.jsx
+++ frontend/src/components/CategoryDiv.jsx
@@ -1,60 +1,68 @@
-import React from 'react'
-import VideoContentCard from './VideoContentCard';
-import ProductsCard from './ProductsCard';
-import TwoVideoCard from './TwoVideoCard';
-import axios from 'axios';
-import { useEffect } from 'react';
-import { useState } from 'react';
+import React from "react";
+import VideoContentCard from "./VideoContentCard";
+import ProductsCard from "./ProductsCard";
+import TwoVideoCard from "./TwoVideoCard";
+import axios from "axios";
+import { useEffect } from "react";
+import { useState } from "react";
 const CategoryDiv = (props) => {
-    const [products,setProducts]=useState([]);
-    
-  
-    useEffect(()=>{
-        async function fetchProducts(){
-            try{
-                let response=await axios.get(`${import.meta.env.VITE_BASE_URL}/products/get-products-by-category/${props.category}`,{
-                   
-                })
-                if(response.status===200){
-                    setProducts(response.data.products.slice(0,4));
+  const [products, setProducts] = useState([]);
 
-                }
-            }catch(err){
-                console.log(err);
-            }
+  useEffect(() => {
+    async function fetchProducts() {
+      try {
+        let response = await axios.get(
+          `${import.meta.env.VITE_BASE_URL}/products/get-products-by-category/${
+            props.category
+          }`,
+          {}
+        );
+        if (response.status === 200) {
+          setProducts(response.data.products.slice(0, 4));
         }
-        if(products.length==0)fetchProducts();
-       
-    },[products])
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    if (products.length == 0) fetchProducts();
+  }, [products]);
   return (
-    
-        <div className="h-full w-full " key={props.index}>
-          <div className="w-[100%] h-full">
-            {/* Video Section */}
-            <VideoContentCard align={props.index%2==0?"left":"right"}
-              videoSrc={"/videoplayback.mp4"}
-              isMobile={props.isMobile}
-              highlight={{
-                heading: "Discover the Latest Trends",
-                description:
-                  "Explore our exclusive collection of fashion and lifestyle products.",
-              }}
-            />
-            
-            <div className="grid grid-cols-1 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-[90%] px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10 ">  
-            {products.length>0 && products.map((product,index)=>{
-              return ( <ProductsCard product={product} key={index} />)
-              
+    <div className="h-full w-full " key={props.index}>
+      <div className="w-[100%] h-full">
+        {/* Video Section */}
+        <VideoContentCard
+          align={props.index % 2 == 0 ? "left" : "right"}
+          videoSrc={"/videoplayback.mp4"}
+          isMobile={props.isMobile}
+          highlight={{
+            heading: "Discover the Latest Trends",
+            description:
+              "Explore our exclusive collection of fashion and lifestyle products.",
+          }}
+        />
 
-            })}
-              
-             
-            </div>
-          </div>
-          {props.isTwoDiv==="true"?<TwoVideoCard isMobile={props.isMobile} videoSrc={"/videoplayback.mp4"}/>:""}
+        <div className="grid grid-cols-1 mx-auto sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 w-[90%] px-2 sm:px-4 md:px-6 lg:px-8 xl:px-10 ">
+          {products.length > 0 ? (
+            products.map((product, index) => {
+              return <ProductsCard product={product} key={index} />;
+            })
+          ) : (
+            <p className="text-gray-500 col-span-full text-center">
+              Loading prooducts...
+            </p>
+          )}
         </div>
-      
-  )
-}
+      </div>
+      {props.isTwoDiv === "true" ? (
+        <TwoVideoCard
+          isMobile={props.isMobile}
+          videoSrc={"/videoplayback.mp4"}
+        />
+      ) : (
+        ""
+      )}
+    </div>
+  );
+};
 
 export default CategoryDiv;
